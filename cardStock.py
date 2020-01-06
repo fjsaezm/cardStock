1e048902a9a023f2621cf9d29f77e1fd974bc382
fjaviersaezm <fjaviersaezm@gmail.com>
2020-01-06 17:12:01 +0100

    Rename file, extended functionality for cards in a file, and fixed cards with commas.

--- cardStock.py
+++ cardStock.py
@@ -1,6 +1,7 @@
 
 import urllib.request
 from bs4 import BeautifulSoup
+import numpy as np
 
 def get_soup(url):
     data = urllib.request.urlopen(url).read().decode()
@@ -9,21 +10,28 @@ def get_soup(url):
     return soup
 
 def get_url(cardName):
-    # Find card
-    card = "+".join(cardName.split(" "))
+    # Find card, remove commas
+    card = cardName
+    if ',' in cardName:
+        card = cardName.replace(",","%2C")
+    card = "+".join(card.split(" "))
+
+    # Search URL, used to get the last reprint of the card
     url = "https://www.cardmarket.com/en/Magic/Products/Search?searchString="+card
 
+    # Get the soup
     soup = get_soup(url)
     # Get Hrefs
     req = soup.find_all('a',href = True)
-    # New split
-    card = "-".join(cardName.split(" "))
+    # New split, removing comma (not in link)
+    card = "-".join(cardName.replace(",","").split(" "))
+    # Get the last reprint in list
     for link in req:
         l = link.get('href')
         if(l.find(card) != -1):
             url = l
             break
-
+    # Get url
     url = "https://www.cardmarket.com"+url
     return url
 
@@ -33,4 +41,11 @@ def get_units(cardName):
     units = soup('dd')[4].string
     print(cardName + " : " + units)
 
-get_units("Steam Vents")
+
+def read_file(filename):
+    f = open(filename,'r').read().split("\n")
+    return f[0:len(f)-1]
+
+cards = read_file("cardList.txt")
+for c in cards:
+   get_units(c)
