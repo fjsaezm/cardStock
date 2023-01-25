3eb67a6a7f7caf7229e947d4701a9cd4955ba48a
fjsaezm <fjaviersaezm@gmail.com>
2023-01-25 20:56:48 +0100

    update utils and prices

--- box.py
+++ box.py
@@ -3,28 +3,46 @@ from cardPrize import get_min_price
 import numpy as np
 
 
+"""
+    Make the script save the prices of the cards after each card computation
+
+    Make the script load the csv with the prices and check if less than 1 day has passed
+    Possibly add argument to force update
+
+    Cards are repeated in the cardlist and double cards appear also as singular.... maybe change json?
+
+
+
+
+
+
+"""
+
 MIN_MYTHICS = 3
 MAX_MYTHICS = 8
+
 
 def dict_min_prices(cardList, edition):
     d = {}
     for c in cardList:
         print(c)
-        d[c] = get_min_price(c,edition)
+        d[c] = get_min_price(c, edition)
 
 
 class Box():
 
     def __init__(self,
-                edition_name : str = "",
-                json_path : str = "",
-                n_boosters : int = 36,
-                card_list : list = None,
-                preloaded_prizes : dict = None) -> None:
-        
+                 edition_name: str = "",
+                 json_path: str = "",
+                 n_boosters: int = 36,
+                 card_list: list = None,
+                 preloaded_prizes: dict = None) -> None:
+
         self.edition = edition_name
         if card_list is None:
             self.cards = read_set_cards(json_path)
+            print(self.cards)
+            exit()
         else:
             self.cards = card_list
 
@@ -33,27 +51,30 @@ class Box():
         self.edition_rares = [a[0] for a in self.cards if a[1] == 'rare']
         self.edition_mythics = [a[0] for a in self.cards if a[1] == 'mythic']
 
-
         if preloaded_prizes is not None:
             self.prices = preloaded_prizes
         else:
-            self.prices = dict_min_prices(self.edition_rares + self.edition_mythics, self.edition)
+            self.prices = dict_min_prices(
+                self.edition_rares + self.edition_mythics, self.edition)
 
-
-        self.num_mythics = np.random.randint(MIN_MYTHICS,MAX_MYTHICS)
+        self.num_mythics = np.random.randint(MIN_MYTHICS, MAX_MYTHICS)
         self.num_rares = self.n_boosters - self.num_mythics
-        self.opened_mythics = np.random.choice(self.edition_mythics, self.num_mythics)
-        self.opened_rares = np.random.choice(self.edition_rares, self.num_rares)
-        self.opened_rare_slot = np.concatenate((self.opened_rares, self.opened_mythics))
+        self.opened_mythics = np.random.choice(
+            self.edition_mythics, self.num_mythics)
+        self.opened_rares = np.random.choice(
+            self.edition_rares, self.num_rares)
+        self.opened_rare_slot = np.concatenate(
+            (self.opened_rares, self.opened_mythics))
 
-        self.total_box_price = np.sum([self.prices[c] for c in self.opened_rare_slot])
+        self.total_box_price = np.sum(
+            [self.prices[c] for c in self.opened_rare_slot])
 
 
 edition_name = "The Brother's War"
 json_path = "set-json/BRO.json"
 
 print("Processing box...")
-ini_box = Box(edition_name = edition_name,json_path=json_path)
+ini_box = Box(edition_name=edition_name, json_path=json_path)
 
 print("Processed box")
 print(ini_box.total_box_price)
@@ -64,7 +85,7 @@ shared_prices = ini_box.prices
 
 exit()
 
-a = [Box(card_list = cards).num_mythics for j in range(0,10000)]
+a = [Box(card_list=cards).num_mythics for j in range(0, 10000)]
 print(min(a))
 print(max(a))
 print(np.mean(a))
--- utils.py
+++ utils.py
@@ -3,33 +3,40 @@ import numpy as np
 import json
 import re
 
+
 def remove_regex_from_string(string, chars):
 
     return re.sub(chars, "", string)
 
+
 def read_file(filename):
-    f = open(filename,'r').read().split("\n")
+    f = open(filename, 'r').read().split("\n")
     cards = [card.split("-") for card in f[:-1]]
     return cards
+
 
 def name_to_url_part(cardName):
     card = cardName
     chars_to_remove = ",|:|'|// "
 
-    card = remove_regex_from_string(card,chars_to_remove)
+    card = remove_regex_from_string(card, chars_to_remove)
 
     card = "-".join(card.split(" "))
 
-    return card    
+    return card
+
 
 def read_set_cards(filename):
     # Cards are read assuming that the JSON comes from:
     # https://mtgjson.com/changelog/mtgjson-v5/
-    
+
     data = json.load(open(filename))
     cards = data['data']['cards']
-    cards = [[a['name'],a['rarity']] for a in cards]
+    cards = [[a['name'], a['rarity']]
+             for a in cards if 'boosterTypes' in a.keys() and a['boosterTypes'] == ['draft']]
 
     return cards
 
-#read_set_cards("set-json/BRO.json")
+
+print(name_to_url_part("Phyrexian Dragon Engine // Mishra, Lost to Phyrexia"))
+# read_set_cards("set-json/BRO.json")
